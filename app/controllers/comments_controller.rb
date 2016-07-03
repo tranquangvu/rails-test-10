99f3da267068f3ebe5fb900834071f878c5dc3ac
Ben Futureworkz <ben.tran@futureworkz.com>
2016-07-04 01:20:28 +0700

    implement create comment and reply

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,12 @@
 class CommentsController < ApplicationController
   def new
+    @comment = Comment.new(parent_id: params[:parent_id])
   end
 
   def create
+    comment = Comment.new(comment_params)
+    comment.user_id = current_user.id
+    @success = comment.save
   end
 
   def edit
@@ -12,5 +16,11 @@ class CommentsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content, :parent_id)
   end
 end
--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,4 +1,37 @@
 require 'rails_helper'
 
 describe CommentsController do  
+  describe '#new' do
+    def do_request
+      xhr :get, :new, parent_id: comment.id, format: :js
+    end
+
+    let!(:user)   { create(:user) }
+    let(:comment) { create(:comment) }
+
+    before do
+      sign_in user
+    end
+
+    it 'assigns a new comment' do
+      do_request
+      expect(assigns(:comment).parent_id).to eq comment.id
+    end
+  end
+
+  describe '#create' do
+    def do_request
+      post :create, comment: attributes_for(:comment), format: :js
+    end
+
+    let!(:user)   { create(:user) }
+
+    before do
+      sign_in user
+    end
+
+    it 'creates a comment' do
+      expect{ do_request }.to change{ Comment.count }.from(0).to(1)
+    end
+  end
 end
